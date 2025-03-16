de75b68c5f85dd319acd20b81b9dbb5600fea532
DanielMartiins <danieldemoraiis@gmail.com>
2025-03-15 21:56:04 -0300

    Exercicios anteriores

--- Plataformas/Kattis/fenwick - Kattis.cpp
+++ Plataformas/Kattis/fenwick - Kattis.cpp
@@ -1,51 +1,41 @@
-//https://open.kattis.com/problems/fenwick
-
 #include <bits/stdc++.h>
 using namespace std;
 
-#define LSOne(S) ((S) & -(S))
+vector<long long> ft; //fenwick tree
 
-typedef vector<long long> vll;
+long long rsq(int j) {
+    if (j == 0) return 0;
+    long long soma = 0;
 
-class FenwickTree {
-    private:
-        vll ft;
-    public:
-        FenwickTree(int m) { ft.assign(m+1, 0);}
-        long long rsq(int j) {
-            if (j == 0) return 0;
-            long long sum = 0;
-            for (; j; j-= LSOne(j))
-                sum += ft[j];
-            return sum;
-        }
-        long long rsq(int i, int j) { return rsq(j) - rsq(i-1);}
+    for (; j; j -= (j & (-j)))
+        soma += ft.at(j);
 
-        void update(int i, long long v) { 
-            for (; i < (int)ft.size(); i += LSOne(i))
-            ft[i] += v;
-        }
-};
+    return soma;
+}
 
-int main(int argc, char const *argv[])
-{
-    int n,q;
+void update(int i, long long v) {
+    for (; i < (int)ft.size(); i += (i & (-i)))
+        ft.at(i) += v;
+}
+
+int main() {
+    int n, q;
     scanf("%d %d", &n, &q);
-    FenwickTree ft(n);
+    ft.assign(n+1, 0);
 
     for (int i = 0; i < q; i++) {
         char op;
-        int index;
+        int index; 
         long long value;
+
         scanf(" %c %d", &op, &index);
 
         if (op == '+') {
             scanf("%lld", &value);
-            ft.update(index + 1, value);
+            update(index + 1, value);
         }
-        else 'printf("%lld\n", ft.rsq(index))';
+        else printf("%lld\n", rsq(index));
     }
-
 
     return 0;
 }
