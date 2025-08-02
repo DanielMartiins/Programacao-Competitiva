716a12a409ba4d8c1ead0a295cd89a7fba727605
DanielMartiins <danieldemoraiis@gmail.com>
2025-08-01 21:53:11 -0300

    [beecrowd] adiciona solução para 2653 - dijkstra

---
+++ Plataformas/beecrowd/beecrowd_2653.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    set<string> joias;
+    string joia;
+    while(cin >> joia) {
+        joias.insert(joia);    
+    }
+    
+    cout << joias.size() << "\n";
+    return 0;
+}
