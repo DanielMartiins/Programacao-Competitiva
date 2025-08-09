c24d2d1f489e771260ce6652a5a81b9eb30f2035
danielmartiins <danieldemoraiis@gmail.com>
2025-08-09 18:44:02 -0300

    refactor(plusMinus): simplifica solução para 'Plus Minus' do HackerRank

--- Plataformas/HackerRank/plusMinus.js
+++ Plataformas/HackerRank/plusMinus.js
@@ -1,48 +1,29 @@
-'use strict';
+let inputString = "";
 
-process.stdin.resume();
-process.stdin.setEncoding('utf-8');
-
-let inputString = '';
-let currentLine = 0;
-process.stdin.on('data', function(inputStdin) {
-    inputString += inputStdin;
+process.stdin.on("data", (data) => {
+	inputString += data;
 });
 
-process.stdin.on('end', function() {
-    inputString = inputString.split('\n');
-
-    main();
+process.stdin.on("end", () => {
+	main();
 });
-
-function readLine() {
-    return inputString[currentLine++];
-}
-
-/*
- * Complete the 'plusMinus' function below.
-*
-* The function accepts INTEGER_ARRAY arr as parameter.
-*/
-
-function plusMinus(arr) {
-    let positives = 0, negatives = 0, zeros = 0;
-    const length = arr.length;
-    for (const num of arr) {
-        if (num > 0) positives++;
-        else if (num < 0) negatives++;
-        else zeros++
-    }
-
-    process.stdout.write((positives/length).toFixed(6) + "\n");
-    process.stdout.write((negatives/length).toFixed(6) + "\n");
-    process.stdout.write((zeros/length).toFixed(6) + "\n");
-}
 
 function main() {
-    const n = parseInt(readLine().trim(), 10);
+	const input = inputString.trim().split("\n");
+	const n = parseInt(input[0]);
+	const arr = input[1].split(" ").map(Number);
 
-    const arr = readLine().replace(/\s+$/g, '').split(' ').map(arrTemp => parseInt(arrTemp, 10));
+	let positives, negatives, zeroes;
+	positives = negatives = zeroes = 0;
+	for (const num of arr) {
+		if (num > 0) positives++;
+		else if (num < 0) negatives++;
+		else zeroes++;
+	}
 
-    plusMinus(arr);
+	console.log(
+		(positives/arr.length).toFixed(6) + '\n' +
+		(negatives/arr.length).toFixed(6) + '\n' +
+		(zeroes/arr.length).toFixed(6)
+	)
 }
