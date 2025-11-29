23254ac427705a1940d8aaa6e534dffae0879d7a
danielmartiins <danieldemoraiis@gmail.com>
2025-11-29 17:20:23 -0300

    [Codeforces] adiciona solução para E. Compatible Numbers

---
+++ Plataformas/Codeforces/Round 112 (Div. 2)/E. Compatible Numbers.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define MAX_BITS 22
+#define MAX_MASK (1 << MAX_BITS)
+#define IMPOSSIBLE -1
+
+int F[MAX_MASK];
+int S[MAX_MASK];
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    int n;
+    cin >> n;
+    int arr[n];
+
+    memset(F, IMPOSSIBLE, sizeof(F));
+    memset(S, IMPOSSIBLE, sizeof(S));
+
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+        F[arr[i]] = arr[i];
+        S[arr[i]] = arr[i];
+    }
+
+    //DP (bottom-up)
+    for (int bit = 0; bit < MAX_BITS; bit++) {
+        for(int mask = 0; mask < MAX_MASK; mask++) {
+            if (mask & (1<<bit))
+                S[mask] = max(S[mask], S[mask ^ (1<<bit)]);
+        }
+    }
+
+    for (int i = 0; i < n; i++) {
+        int invertido = arr[i] ^ ((1 << MAX_BITS) - 1);
+        if (i == 0) 
+            cout << S[invertido];
+        else 
+            cout << " " << S[invertido];
+    }
+    cout << "\n";
+}
